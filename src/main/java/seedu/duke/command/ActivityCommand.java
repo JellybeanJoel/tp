181e9d8fa5b02509a5edc30ab06a2cbb010b6da5
Weng Xian Yang <e1398191@u.nus.edu>
2026-03-19 12:28:04 +0800

    Revert "Merge branch-Trip"

--- src/main/java/seedu/duke/command/ActivityCommand.java
+++ src/main/java/seedu/duke/command/ActivityCommand.java
@@ -6,18 +6,8 @@ public abstract class ActivityCommand {
 
     protected ActivityList activityList;
 
-    private static boolean isTripOpen = false;
-
     public ActivityCommand(ActivityList activityList) {
         this.activityList = activityList;
-
-        if (!isTripOpen) {
-            throw new IllegalStateException("No trip is currently open. Please open a trip to manage activities.");
-        }
-    }
-
-    public static void setTripOpen(boolean isOpen) {
-        isTripOpen = isOpen;
     }
 
     public abstract String execute(String tripName);
